708fd97c82e2ce53ae0e511a14d639fadd255808
Artem Zakharchenko <kettanaito@gmail.com>
2019-02-27 12:24:54 +0100

    Adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,13 +36,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Redd`,
+        short_name: `Redd`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#eb4a47`,
+        theme_color: `#eb4a47`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/redd-icon.png`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-styled-components',
