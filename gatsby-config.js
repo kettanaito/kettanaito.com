278fea32b38893dbce9d59a534b0c80a46d579cd
Artem Zakharchenko <kettanaito@gmail.com>
2019-02-27 09:56:40 +0100

    Adds "gatsby-plugin-styled-components"

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-styled-components',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
