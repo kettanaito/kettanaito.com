27e2b12f0a4865f14b12f8d92f0fc7205c96dd51
Artem Zakharchenko <kettanaito@gmail.com>
2019-03-02 15:31:33 +0100

    Use custom names

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
-import Footer from './footer'
-import './styles.css'
+import Header from './PageHeader'
+import Footer from './PageFooter'
+import './layout.css'
 import Container from './Container'
 
 const Layout = ({ children }) => (
