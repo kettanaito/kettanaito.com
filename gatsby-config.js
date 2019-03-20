77560cee93f55b398baf054a79798981e0c61cf1
Artem Zakharchenko <kettanaito@gmail.com>
2019-03-20 15:54:34 +0100

    Adds Google Analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-136640393-1',
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
