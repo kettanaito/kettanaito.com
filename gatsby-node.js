dcdc6a4caa40cd6f72c1eee2321c58a5a298bf39
Artem Zakharchenko <kettanaito@gmail.com>
2019-03-24 11:31:02 +0100

    Serves posts under "blog/" path

--- gatsby-node.js
+++ gatsby-node.js
@@ -72,7 +72,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     createNodeField({
       node,
       name: 'url',
-      value: path.normalize(`posts/${postSlug}`),
+      value: path.normalize(`blog/${postSlug}`),
     })
   }
 }
