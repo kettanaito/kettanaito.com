39bc06b3237785d4e03b9dbf0547dda21953463b
kettanaito <kettanaito@gmail.com>
2019-05-30 09:31:30 +0200

    Code: Increase border-radius

--- src/PrismTheme.js
+++ src/PrismTheme.js
@@ -1,7 +1,7 @@
 export default {
   'code[class*="language-"]': {
     fontFamily:
-      'Fira Mono, Consolas, Menlo, Monaco, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", "Courier New", Courier, monospace',
+      '"Roboto Mono", "Fira Mono", Consolas, Menlo, Monaco, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", "Courier New", Courier, monospace',
     fontSize: '16px',
     lineHeight: '1.45',
     direction: 'ltr',
@@ -22,7 +22,7 @@ export default {
   },
   'pre[class*="language-"]': {
     fontFamily:
-      'Consolas, Menlo, Monaco, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", "Courier New", Courier, monospace',
+      '"Roboto Mono", "Fira Mono", Consolas, Menlo, Monaco, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", "Courier New", Courier, monospace',
     fontSize: '16px',
     lineHeight: '1.375',
     direction: 'ltr',
