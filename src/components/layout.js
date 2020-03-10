32efd32af3d9e61484db4a8c4de82b3b4bb3795b
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-10 22:42:27 +0100

    Adds basic header layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { ThemeProvider } from 'styled-components'
 
+import theme from '../theme'
 import Header from './PageHeader'
 import Footer from './PageFooter'
 import './layout.css'
@@ -20,13 +22,13 @@ const Layout = ({ children }) => (
       }
     `}
     render={(data) => (
-      <>
+      <ThemeProvider theme={theme}>
         <Header siteTitle={data.site.siteMetadata.title} />
         <Container paddingVertical={2} paddingVerticalMd={4}>
           <main>{children}</main>
         </Container>
         <Footer />
-      </>
+      </ThemeProvider>
     )}
   />
 )
