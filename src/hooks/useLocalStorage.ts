68b62af5f9568d23262fbbb28c64d4642e638110
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-13 14:45:09 +0100

    useLocalStorage: Fallbacks to null on SSR

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,7 +3,9 @@ import { useState } from 'react'
 export const useLocalStorage = (
   name: string
 ): [string, (value: string) => void] => {
-  const [value, setValue] = useState(localStorage.getItem(name))
+  const [value, setValue] = useState(
+    typeof window === 'undefined' ? null : localStorage.getItem(name)
+  )
 
   const setStorageValue = (value: string) => {
     localStorage.setItem(name, value)
