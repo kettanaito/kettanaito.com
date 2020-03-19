12c88eb28f8ac2d9d682f499cf3f679c3d6847ed
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-19 09:47:35 +0100

    Adds dark code theme for dark mode

--- src/components/mdx/Code.tsx
+++ src/components/mdx/Code.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { Box } from 'atomic-layout'
 import { Code as ReactCdxCode } from 'react-cdx'
+import DarkTheme from 'prism-react-renderer/themes/dracula'
+import { ThemeContext } from '../ThemeContext'
 
 const CodeWrapper = styled.div`
   box-shadow: 0 1.6px 2px rgba(0, 0, 0, 0.05), 0 3px 4px rgba(0, 0, 0, 0.04),
@@ -9,13 +11,18 @@ const CodeWrapper = styled.div`
     0 24px 32px rgba(0, 0, 0, 0.02);
 `
 
-export const Code = ({ children, language, className, ...props }) => (
-  <Box as={CodeWrapper} marginVerticalMd={4}>
-    <ReactCdxCode
-      {...props}
-      code={children}
-      language={language || (className && className.replace('language-', ''))}
-      showLineNumbers={true}
-    />
-  </Box>
-)
+export const Code = ({ children, language, className, ...props }) => {
+  const { themeName } = useContext(ThemeContext)
+
+  return (
+    <Box as={CodeWrapper} marginVerticalMd={4}>
+      <ReactCdxCode
+        {...props}
+        code={children}
+        language={language || (className && className.replace('language-', ''))}
+        showLineNumbers={true}
+        {...(themeName === 'dark' ? { theme: DarkTheme as any } : {})}
+      />
+    </Box>
+  )
+}
