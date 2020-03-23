7276e26523bd0ebad17cc4b876979d73a3227ac9
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-23 09:39:34 +0100

    Single post: Provides responsive for post meta section

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,7 +1,12 @@
+import { Box } from 'atomic-layout'
 import styled from 'styled-components'
 
-export const Label = styled.span`
+export const Label = styled(Box)`
   font-weight: 500;
   color: ${({ theme }) => theme.colors.gray};
   line-height: 1.4;
 `
+
+Label.defaultProps = {
+  as: 'span',
+}
