70f3f9630c63cdab1e17839af266e8b8e2220ae9
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-24 09:14:00 +0100

    Sets clear boundaries between theme-related hooks

--- src/components/ThemeContext.tsx
+++ src/components/ThemeContext.tsx
@@ -1,6 +1,7 @@
 import { createContext } from 'react'
+import { ColorScheme } from '../themes/ColorScheme'
 
-export const ThemeContext = createContext({
-  themeName: null,
-  toggleTheme: () => null,
-})
+export const ThemeContext = createContext<{
+  themeName: ColorScheme
+  updateThemeMode: (nextValue: ColorScheme) => void
+}>(null)
