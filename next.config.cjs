dd43396bf0f0a4344684a9ae34381a77a6d22e6e
Artem Zakharchenko <kettanaito@gmail.com>
2023-10-20 10:37:32 +0200

    add /discord redirect

--- next.config.cjs
+++ next.config.cjs
@@ -1,7 +1,24 @@
+const { invariant } = require('outvariant')
+
+const { DISCORD_INVITE_URL } = process.env
+
+invariant(
+  DISCORD_INVITE_URL,
+  'Failed to load: the "DISCORD_INVITE_URL" environment variable is missing'
+)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/discord',
+        destination: DISCORD_INVITE_URL,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
