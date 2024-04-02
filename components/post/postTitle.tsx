c17bd0233f04696b93d7a93c734b842cb9ade4b1
Jimmy <jimmyrss1102@gmail.com>
2024-04-03 03:29:13 +0800

    feat: add title anchor

--- components/post/postTitle.tsx
+++ components/post/postTitle.tsx
@@ -1,3 +1,5 @@
 export function PostTitle({ title }: { title: string }): JSX.Element {
-  return <h1 className="text-4xl lg:text-6xl font-extrabold">{title}</h1>
+  return <h1 id={title} className="text-4xl lg:text-6xl font-extrabold">
+    <a href={`#${title}`} className="hover:underline">{title}</a>
+  </h1>
 }
