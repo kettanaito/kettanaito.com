f68d589a7de7c56822665a3d478f72646b8efd0d
Artem Zakharchenko <kettanaito@gmail.com>
2024-05-07 19:00:21 +0200

    update og images

--- components/post/postTitle.tsx
+++ components/post/postTitle.tsx
@@ -1,3 +1,7 @@
 export function PostTitle({ title }: { title: string }): JSX.Element {
-  return <h1 className="text-4xl lg:text-6xl font-extrabold">{title}</h1>
+  return (
+    <h1 className="text-4xl lg:text-6xl font-extrabold text-balance">
+      {title}
+    </h1>
+  )
 }
